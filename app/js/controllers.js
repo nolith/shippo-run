c623641bdef8cfb29ad59fe7dd7b61e95fde444a
Alessio Caiazza <nolith@abisso.org>
2013-02-07 17:13:15 +0100

    Shippo Asakura Speed

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,34 @@
 /* Controllers */
 
 
+function Shippo($scope) {
+	//$scope.run = 1000;
+	$scope.max = 25000;
+	$scope.sec1fat = 600;
+	$scope.min1fat = $scope.sec1fat/60;
+
+	$scope.ki_pool=81;
+	$scope.fatigues=9;
+
+	$scope.distance=500;
+
+	$scope.turns = function() {
+		return $scope.distance * 1000 / $scope.max;
+	}
+	$scope.spent_fatigues = function() {
+		return Math.floor($scope.turns() * 3 / $scope.sec1fat);
+	}
+	$scope.trip_m = function() { return $scope.turns() * 3 / 60; }
+	$scope.spent_ki = function() { return Math.floor($scope.turns() / 10); }
+	$scope.recover_h = function() {
+		var fat_recover_as_ki = $scope.spent_fatigues() * 5; 
+		var ki_recover = (18 + fat_recover_as_ki + $scope.spent_ki())/6;
+
+		return Math.ceil(ki_recover); 
+	}
+}
+Shippo.$inject= ['$scope'];
+
 function MyCtrl1() {}
 MyCtrl1.$inject = [];
 
